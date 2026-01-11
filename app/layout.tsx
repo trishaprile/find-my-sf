f167f1e6bf8a44a0a136d7264b3eabf12b05866c
Trisha Le <trishaprilee@gmail.com>
2026-01-11 14:12:15 -0800

    add preview image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,21 @@ const gaegu = Gaegu({
 export const metadata: Metadata = {
   title: 'find my sf',
   description: 'Discover events happening in San Francisco!',
+  openGraph: {
+    title: 'find my sf',
+    description: 'Discover events happening in San Francisco!',
+    url: 'https://findmysf.com',
+    siteName: 'find my sf',
+    images: [
+      {
+        url: '/preview-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
